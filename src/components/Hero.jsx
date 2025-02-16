bc382d729dd34b9fc2b418a2b02bafd9613e182c
GaetanAL <lohiergaetan.pro@gmail.com>
2025-02-16 11:22:05 -0500

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
                     <div className="flex items-center gap-3">
                         <figure className="img-box w-9 h-9 rounded-lg ">
                             <img
-                                src="/images/avatar.png"
+                                src="public/images/avatar.png"
                                 width={40}
                                 height={40}
                                 alt="Portrait"
